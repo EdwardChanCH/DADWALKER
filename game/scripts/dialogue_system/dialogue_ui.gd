c7cdd9bf1761c3fc8b76f89c2c8af803f2ad9158
Bee <zhikanchen8@gmail.com>
2025-08-07 19:04:23 -0400

    Added border

--- game/scripts/dialogue_system/dialogue_ui.gd
+++ game/scripts/dialogue_system/dialogue_ui.gd
@@ -18,6 +18,8 @@ extends Control
 
 @export var text_box_animation_player: AnimationPlayer
 
+@export var border_animation_player : AnimationPlayer
+
 # The current index of the dialogue sequance
 var __current_dialogue_index: int = 0
 
@@ -27,11 +29,8 @@ var __current_dialogue_index: int = 0
 var __current_character_position: Array[_DialogueSequence.Characters] = [_DialogueSequence.Characters.NONE, _DialogueSequence.Characters.NONE]
 
 func _ready() -> void:
-	
-	# Character enter -> text box slide in -> display line
-	
+	await play_ui_slide_in_animation();
 	reset_dialogue_sequance()
-	text_box_animation_player.play("slide_in")
 	pass
 
 func _gui_input(event: InputEvent) -> void:
@@ -45,6 +44,7 @@ func _gui_input(event: InputEvent) -> void:
 		var keep_going: bool = await set_dialogue_sequence(__current_dialogue_index)
 		
 		if ( not keep_going ):
+			await play_ui_slide_out_animation()
 			visible = false
 		print("Clicked")
 		
@@ -67,31 +67,29 @@ func set_dialogue_sequence(new_sequence: int) -> bool:
 	if(current_sequence is _TextSequence):
 		var character_position = set_text(current_sequence)
 		if(character_position != _DialogueSequence.Position.NONE):
-			await play_character_sprite_animation(character_position, "talk").animation_finished
+			await play_sprite_ui_animation(character_position, "talk").animation_finished
 			await get_tree().create_timer(0.25).timeout
 		return true
 	
 	if(current_sequence is _CharacterSequence):
-		set_character_sprite(current_sequence)
-		await play_character_sprite_animation(current_sequence.character_position, "slide_in").animation_finished
-		await get_tree().create_timer(0.5).timeout
+		if(current_sequence.play_animation):
+			await play_sprite_ui_animation(current_sequence.character_position, "slide_in", true).animation_finished
+			set_character_sprite(current_sequence)
+			await play_sprite_ui_animation(current_sequence.character_position, "slide_in").animation_finished
+		else:
+			set_character_sprite(current_sequence)
+
+		__current_dialogue_index += 1
+		set_dialogue_sequence(__current_dialogue_index)
+		return true
+
+	if(current_sequence is _SpriteAniatmionSequence):
+		await play_sprite_ui_animation(current_sequence.target_position, current_sequence.animation_name).animation_finished
 		__current_dialogue_index += 1
 		set_dialogue_sequence(__current_dialogue_index)
 		return true
 
 	return false
-
-func play_character_sprite_animation(character_position: _DialogueSequence.Position, animation_name: String) -> AnimationPlayer:
-	
-	var target_animation_player: AnimationPlayer = null
-	match(character_position):
-		_DialogueSequence.Position.LEFT:
-			target_animation_player = character_sprite_animation_player_1
-			
-		_DialogueSequence.Position.RIGHT:
-			target_animation_player = character_sprite_animation_player_2
-	target_animation_player.play(animation_name)
-	return target_animation_player
 
 ## Set character sprite
 func set_character_sprite(sequence: _CharacterSequence) -> void:
@@ -109,7 +107,7 @@ func set_character_sprite(sequence: _CharacterSequence) -> void:
 			
 	if( not target_sprite ):
 		return
-	
+
 	## This is hardcoded to save time
 	## It would be better to have a character class for storing sprite and their expression
 	## Might actually do that later if needed
@@ -123,6 +121,7 @@ func set_character_sprite(sequence: _CharacterSequence) -> void:
 			target_sprite.texture = character_sprite_texture[2]
 		_:
 			target_sprite.texture = null
+	
 	pass
 
 func set_text(sequence: _TextSequence) -> _DialogueSequence.Position:
@@ -134,12 +133,11 @@ func set_text(sequence: _TextSequence) -> _DialogueSequence.Position:
 	if(character_name != "None"):
 		character_name_label.text = character_name.capitalize()
 	
-	
 	if(__current_character_position[0] == sequence.character_name):
 		character_sprite_1.modulate = Color(1, 1, 1)
 		character_sprite_2.modulate = Color(0.5, 0.5, 0.5)
 		return _DialogueSequence.Position.LEFT
-		
+
 	if (__current_character_position[1] == sequence.character_name):
 		character_sprite_1.modulate = Color(0.5, 0.5, 0.5)
 		character_sprite_2.modulate = Color(1, 1, 1)
@@ -148,3 +146,44 @@ func set_text(sequence: _TextSequence) -> _DialogueSequence.Position:
 	character_sprite_1.modulate = Color(0.5, 0.5, 0.5)
 	character_sprite_2.modulate = Color(0.5, 0.5, 0.5)
 	return _DialogueSequence.Position.NONE
+
+func play_sprite_ui_animation(character_position: _DialogueSequence.Position, animation_name: String, play_backwards: bool = false) -> AnimationPlayer:
+	var target_animation_player: AnimationPlayer = null
+	match(character_position):
+		_DialogueSequence.Position.LEFT:
+			target_animation_player = character_sprite_animation_player_1
+			
+		_DialogueSequence.Position.RIGHT:
+			target_animation_player = character_sprite_animation_player_2
+	
+	if(play_backwards):
+		target_animation_player.play_backwards(animation_name)
+	else:
+		target_animation_player.play(animation_name)
+		
+	return target_animation_player
+
+func play_ui_slide_in_animation() -> Signal:
+	
+	set_character_sprite(dialogue.starting_characters_1)
+	set_character_sprite(dialogue.starting_characters_2)
+	
+	border_animation_player.play("slide_in")
+	character_sprite_animation_player_1.play("slide_in")
+	character_sprite_animation_player_2.play("slide_in")
+	text_box_animation_player.play("slide_in")
+	await character_sprite_animation_player_2.animation_finished
+	return get_tree().create_timer(0.25).timeout
+
+func play_ui_slide_out_animation() -> Signal:
+	
+	border_animation_player.play_backwards("slide_in")
+	character_sprite_animation_player_1.play_backwards("slide_in")
+	character_sprite_animation_player_2.play_backwards("slide_in")
+	text_box_animation_player.play_backwards("slide_in")
+	await character_sprite_animation_player_2.animation_finished
+	
+	border_animation_player.play_backwards("slide_in")
+	await border_animation_player.animation_finished
+	
+	return get_tree().create_timer(0.25).timeout
