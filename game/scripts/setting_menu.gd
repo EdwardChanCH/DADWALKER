008697aad64611efc8478ac3339f718be84c4499
Bee <zhikanchen8@gmail.com>
2025-08-11 14:14:04 -0400

    Minor refactoring

--- game/scripts/setting_menu.gd
+++ game/scripts/setting_menu.gd
@@ -9,25 +9,25 @@ extends CanvasLayer
 @export var voice_slider: Slider
 
 func _ready() -> void:
-	var master_arg = [master_slider, _AudioManager.AudioType.MASTER]
+	var master_arg = [master_slider, AudioManager.AudioType.MASTER]
 	master_slider.drag_ended.connect(Callable(update_volume).bind(master_arg))
 	
-	var sfx_arg = [sfx_slider, _AudioManager.AudioType.SFX]
+	var sfx_arg = [sfx_slider, AudioManager.AudioType.SFX]
 	sfx_slider.drag_ended.connect(Callable(update_volume).bind(sfx_arg))
 	
-	var ui_arg = [ui_slider, _AudioManager.AudioType.UI]
+	var ui_arg = [ui_slider, AudioManager.AudioType.UI]
 	ui_slider.drag_ended.connect(Callable(update_volume).bind(ui_arg))
 	
-	var music_arg = [music_slider, _AudioManager.AudioType.MUSIC]
+	var music_arg = [music_slider, AudioManager.AudioType.MUSIC]
 	music_slider.drag_ended.connect(Callable(update_volume).bind(music_arg))
 	
-	var voice = [voice_slider, _AudioManager.AudioType.VOICE]
+	var voice = [voice_slider, AudioManager.AudioType.VOICE]
 	voice_slider.drag_ended.connect(Callable(update_volume).bind(voice))	
 	pass
 	
 func update_volume(_changed: bool, arg_array: Array) -> void:
 	var slider: Slider = arg_array[0] as Slider
-	_AudioManager.set_volume(arg_array[1], slider.value)
+	AudioManager.set_volume(arg_array[1], slider.value)
 	pass
 
 
