75f3bf64fc7ef433d8ab51db9854b69cbd69e69d
Bee <zhikanchen8@gmail.com>
2025-08-14 16:22:06 -0400

    Pause menu function

--- game/scripts/pause_menu.gd
+++ game/scripts/pause_menu.gd
@@ -26,3 +26,20 @@ func _on_visibility_changed() -> void:
 		get_tree().paused = false
 		
 	pass # Replace with function body.
+
+
+func _on_play_button_pressed() -> void:
+	
+	pass # Replace with function body.
+
+
+func _on_setting_button_pressed() -> void:
+	AudioManager.play_sfx("res://assets/sounds/sfx/sfx_ui_confirm_fd1.ogg", 0.5)
+	Globals.setting_menu.visible = true
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	AudioManager.play_sfx("res://assets/sounds/sfx/sfx_ui_confirm_fd1.ogg", 0.5)
+	Globals.credit_menu.visible = true
+	pass # Replace with function body.
